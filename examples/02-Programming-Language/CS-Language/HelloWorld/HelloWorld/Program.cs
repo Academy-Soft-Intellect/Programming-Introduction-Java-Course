49ea045f07c67f0cc79f3b8ab9bf4d450bec994d
sestrimski <sestrimski@gmail.com>
2015-10-24 11:56:01 +0300

    Lecture 4 and Examples; Fixes in Examples of Lecture 2 and 3

--- examples/02-Programming-Language/CS-Language/HelloWorld/HelloWorld/Program.cs
+++ examples/02-Programming-Language/CS-Language/HelloWorld/HelloWorld/Program.cs
@@ -11,9 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine();
-            Console.WriteLine("Hi");
-            Console.Read();
+ 
         }
     }
 }
