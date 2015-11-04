d177d73cfca2986f8115ef07feaaaab084c83272
sestrimski <sestrimski@gmail.com>
2015-11-04 11:38:22 +0200

    Updates

--- examples/05-Arrays-Part-1/Arrays/src/ArraysDemo2.java
+++ examples/05-Arrays-Part-1/Arrays/src/ArraysDemo2.java
@@ -2,7 +2,7 @@ public class ArraysDemo2 {
 
 	public static void main(String[] args) {
 		
-		String[] names = { "Peter", "Maria", "Katya", "Todor" };
+		String[] names = new String[]{ "Peter", "Maria", "Katya", "Todor" };
 		
 		for (int i = 0; i < names.length; i++) {
 			System.out.println("names[" + i + "] = " + names[i]);
---
+++ examples/05-Arrays-Part-1/Arrays/src/DeepCopyDemo.java
@@ -0,0 +1,39 @@
+public class DeepCopyDemo {
+	
+	public static void main(String[] args) {
+	
+		int[] oldArray = new int []{ 1, 2, 3, 4, 5, 7 };	
+		
+		int[] newArray = new int[oldArray.length];
+
+		for(int i = 0; i< oldArray.length; i++){
+			newArray[i] = oldArray[i];
+		}
+		
+		for(int i = 0; i< oldArray.length; i++){
+			System.out.print(oldArray[i] + " ");
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i< newArray.length; i++){
+			System.out.print(newArray[i] + " ");
+		}
+		
+		oldArray[0] = 20;
+		
+		System.out.println();
+		
+		for(int i = 0; i< oldArray.length; i++){
+			System.out.print(oldArray[i] + " ");
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i< newArray.length; i++){
+			System.out.print(newArray[i] + " ");
+		}
+		
+	
+	}
+}
